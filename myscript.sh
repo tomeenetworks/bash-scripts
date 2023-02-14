f74ee8ebef233c63acd227312a87951a8fcbb932
tomeenetworks <tomeenetworks.gmail.com>
2023-02-14 21:04:44 +0100

    added myscript

---
+++ myscript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+# echo command
+echo Hello world!
+
+#variables
+#upppercase by convention
+#letters, numbers, underscores
+
+NAME="Tomi"
+NAME2="Anti"
+echo "My name is ${NAME}"
+echo "My name is $1 $2 "
+
+
+
